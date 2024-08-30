c43e13ff63577eeb712db9ec5318d1d6a2ad5d2b
Radeel Ahmad <160111777+RadeelAhmad@users.noreply.github.com>
2024-08-31 02:14:38 +0500

    Update Type.js

--- Type.js
+++ Type.js
@@ -6,11 +6,12 @@ function Type() {
     <Typewriter
       options={{
         strings: [
-          "Software Developer",
-          "Freelancer",
-          "MERN Stack Developer",
-          "Open Source Contributor",
-        ],
+          "Programmer",
+          "CTF Player",
+          "Red Teamer",
+          "Networking",
+          "Gamer",
+    ],
         autoStart: true,
         loop: true,
         deleteSpeed: 50,
